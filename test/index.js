bd16ece859c57d7f02a21aa1a6f0d9cceff4725b
Miguh Ruiz <miguhruiz@gmail.com>
2016-07-17 19:18:11 +0200

    [Add] Function and test to insert data into an specific table

--- test/index.js
+++ test/index.js
@@ -9,3 +9,11 @@ test('Connect to the database', async t => {
   let connection = await adapter.connect()
   t.deepEqual(connection.host, config.db.host)
 })
+
+test('Insert data into an specific table', async t => {
+  let body = {user: 'miguhruiz'}
+  let table = 'users'
+
+  let result = await adapter.insert(body, table)
+  t.deepEqual(result.inserted, 1)
+})
